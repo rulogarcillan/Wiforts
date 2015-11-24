885d5346fc4564557821808cbd7b9680a335f392
Rulo <raulrcs@gmail.com>
2015-11-24 15:30:13 +0100

    Update Connectivity.java

--- app/src/main/java/com/r/raul/tools/Connectivity.java
+++ app/src/main/java/com/r/raul/tools/Connectivity.java
@@ -9,6 +9,12 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.telephony.TelephonyManager;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 import java.util.List;
 
 
@@ -85,5 +91,19 @@ public class Connectivity {
         } else {
             return a.getString(R.string.nodisponible);
         }
+    }
+
+    public static String getPublicIp() throws IOException {
+        return getPublicIp(false);
+    }
+    
+    public static String getPublicIp(boolean useHttps) throws IOException {
+        URL ipify = useHttps ? new URL("https://api.ipify.org") : new URL("http://api.ipify.org");
+        URLConnection conn = ipify.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String ip = null;
+        ip = in.readLine();
+        in.close();
+        return ip;
     }
 }
