68f2a80135493647e1dc7ab4bf2215b8da07d13a
Rulo <raulrcs@gmail.com>
2015-11-28 01:10:56 +0100

    Nuevos dearrollo

--- app/src/main/java/com/r/raul/tools/MainActivity.java
+++ app/src/main/java/com/r/raul/tools/MainActivity.java
@@ -24,8 +24,6 @@ public class MainActivity extends AppCompatActivity
 
     MainDeviceInfo fragmentD;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
