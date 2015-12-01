602140bc0dae3b361589ab22762a0c3a57e877c5
Rulo <raulrcs@gmail.com>
2015-12-01 21:41:15 +0100

    Nuevos dearrollo

--- app/src/main/java/com/r/raul/tools/MainDeviceInfo.java
+++ app/src/main/java/com/r/raul/tools/MainDeviceInfo.java
@@ -14,6 +14,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.CardView;
--- app/src/main/java/com/r/raul/tools/MainOpenPorts.java
+++ app/src/main/java/com/r/raul/tools/MainOpenPorts.java
@@ -1,10 +1,15 @@
 package com.r.raul.tools;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 
 public class MainOpenPorts extends Fragment {
@@ -19,6 +24,10 @@ public class MainOpenPorts extends Fragment {
 
     }
 
+    Button btnGo;
+    RecyclerView recyclerView;
+    TabLayout tabLayout;
+  LinearLayout filtros;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,6 +37,49 @@ public class MainOpenPorts extends Fragment {
 
         View rootView = inflater.inflate(R.layout.open_ports, container, false);
 
+        tabLayout = (TabLayout) rootView.findViewById(R.id.appbartabs);
+        tabLayout.setTabMode(TabLayout.MODE_FIXED);
+        tabLayout.addTab(tabLayout.newTab().setText("Filtros"));
+        tabLayout.addTab(tabLayout.newTab().setText("Abiertos"));
+        tabLayout.addTab(tabLayout.newTab().setText("Cerrados"));
+
+        btnGo = (Button) rootView.findViewById(R.id.btnGo);
+        filtros=(LinearLayout) rootView.findViewById(R.id.filtros);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.rePorts);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(llm);
+
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if (tab.getPosition() == 0) {
+                    recyclerView.setVisibility(View.INVISIBLE);
+                    filtros.setVisibility(View.VISIBLE);
+                } else {
+                    recyclerView.setVisibility(View.VISIBLE);
+                    filtros.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        CustomAdapter a = new CustomAdapter();
+
+
+        recyclerView.setAdapter(a);
         return rootView;
     }
 }
