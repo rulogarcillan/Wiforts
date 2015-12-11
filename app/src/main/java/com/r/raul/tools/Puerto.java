f797bef9797d90391f48611fa89b170156fc1121
Rulo <raulrcs@gmail.com>
2015-12-11 14:39:15 +0100

    Update Puerto.java

--- app/src/main/java/com/r/raul/tools/Puerto.java
+++ app/src/main/java/com/r/raul/tools/Puerto.java
@@ -28,3 +28,60 @@ public static class Puerto {
     }
 
 }
+
+
+package es.caser.campanaretencion.base.campana.test;
+
+import java.util.ArrayList;
+
+public class ordena {
+
+	public ordena() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+/*
+	public boolean parsea(String lista){
+	
+		ArrayList<Integer> listaPuertos = new ArrayList<Integer>();
+		
+		if (lista.contains(",")){	
+			String[] partsComa = lista.split(",");
+			for (String portsComa : partsComa){
+				if (portsComa.contains("-")){
+					String[] partsGuion = portsComa.split("-");
+					if (partsGuion.length == 2){
+						for (int i = Integer.parseInt(partsGuion[0]); i = Integer.parseInt(partsGuion[1]); i++){
+							listaPuertos.add(i);
+						}
+					
+					}else{
+						return error;
+					}			
+					
+				}else{		
+					listaPuertos.add(portsComa);			
+				}	
+			}
+		}else if (string.contains("-")){
+					String[] partsGuion = string.split("-");
+					If (partsGuion.count = 2){
+						for (int i = partsGuion[0]; i = partsGuion[0]; i++){
+							listaPuertos.add(i);
+						}			
+					}else{
+						return error;
+					}			
+					
+				}
+		}else{
+		listaPuertos.add(string);
+		}
+		
+	}
+
+
+	
+}	*/
