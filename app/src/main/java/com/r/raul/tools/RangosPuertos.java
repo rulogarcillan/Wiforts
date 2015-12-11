0d643d46482bc8694fab80319f52330d8465e25b
Rulo <raulrcs@gmail.com>
2015-12-11 09:50:15 +0100

    Create RangosPuertos.java

---
+++ app/src/main/java/com/r/raul/tools/RangosPuertos.java
@@ -0,0 +1,25 @@
+
+public class RangosPuertos {
+	
+	int idNombre;
+	String rangoPuertos;	
+	
+	public RangosPuertos(int idNombre, String rangoPuertos) {
+		super();
+		this.idNombre = idNombre;
+		this.rangoPuertos = rangoPuertos;
+	}
+	public int getIdNombre() {
+		return idNombre;
+	}
+	public void setIdNombre(int idNombre) {
+		this.idNombre = idNombre;
+	}
+	public String getRangoPuertos() {
+		return rangoPuertos;
+	}
+	public void setRangoPuertos(String rangoPuertos) {
+		this.rangoPuertos = rangoPuertos;
+	}
+	
+}
