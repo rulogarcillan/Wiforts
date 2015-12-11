e968f4c64a6acd0ebc99467afaaa382eb5e89524
Rulo <raulrcs@gmail.com>
2015-12-11 09:14:35 +0100

    Update AnalizaPuerto.java

--- app/src/main/java/com/r/raul/tools/AnalizaPuerto.java
+++ app/src/main/java/com/r/raul/tools/AnalizaPuerto.java
@@ -5,21 +5,29 @@ import com.r.raul.tools.Puerto;
 import java.util.concurrent.Callable;
 
 public class AnalizaPuerto implements Callable<Puerto> {
-
-	private int puertoTratar;
 	
-	public AnalizaPuerto(int puertoTratar){
+	private String ip;
+	private int puertoTratar;
+	private int timeOut;
+	
+	public AnalizaPuerto(String ip, int puertoTratar, int timeOut){
+		this.ip = ip;
 		this.puertoTratar = puertoTratar;
+		this.timeOut = timeOut;
 	}
 
 
 	@Override
-	public Puerto call() throws Exception {
-		Puerto puerto = new Puerto(puertoTratar);
-		
-		//tratamiento
-		
-		return puerto;
+	public Puerto call() {
+	   //tratamiento
+	   try {
+                Socket socket = new Socket();
+                socket.connect(new InetSocketAddress(ip, puertoTratar), timeOut);
+                socket.close();
+                return new Puerto(puertoTratar, true);
+            } catch (Exception ex) {
+                return new Puerto(puertoTratar, false);
+            }
 	}
 	
 }
