2f013b464100de5373aaa5cff9ae219a13976dc2
Rulo <raulrcs@gmail.com>
2015-12-16 13:12:42 +0100

    Create DetalleTarjeta.java

---
+++ app/src/main/java/com/r/raul/tools/Device/DetalleTarjeta.java
@@ -0,0 +1,39 @@
+
+
+package com.r.raul.tools.Device;
+
+import java.util.ArrayList;
+
+public class DetalleTarjeta {
+
+	private ArrayList<DetalleFilaTarjeta> infoIp;
+	private ArrayList<DetalleFilaTarjeta> infoRed;
+
+	public DetalleTarjeta() {
+		super();
+		infoIp = new ArrayList<DetalleFilaTarjeta>;
+		infoRed = new ArrayList<DetalleFilaTarjeta>;
+	}
+	
+	public ArrayList<DetalleFilaTarjeta> getInfoIp() {
+		return infoIp;
+	}
+	
+	public void setInfoIp(ArrayList<DetalleFilaTarjeta> infoIp) {
+		this.infoIp = infoIp;
+	}
+	
+	public ArrayList<DetalleFilaTarjeta> getInfoRed() {
+		return infoRed;
+	}
+	
+	public void setInfoRed(ArrayList<DetalleFilaTarjeta> infoRed) {
+		this.infoRed = infoRed;
+	}
+	
+	
+	
+
+	
+
+}
