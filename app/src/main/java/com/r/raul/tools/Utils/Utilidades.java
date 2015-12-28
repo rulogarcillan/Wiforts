b6900d9550f65d9c0a4ef191975e702ff634b034
Rulo <raulrcs@gmail.com>
2015-12-28 11:50:31 +0100

    Update Utilidades.java

--- app/src/main/java/com/r/raul/tools/Utils/Utilidades.java
+++ app/src/main/java/com/r/raul/tools/Utils/Utilidades.java
@@ -52,7 +52,6 @@ public class Utilidades {
 
     }
 
-
     public static Future<Machine> machineExist(final ExecutorService es, final InetAddress ip, final int time) {
 
         return es.submit(new Callable<Machine>() {
@@ -79,8 +78,8 @@ public class Utilidades {
 
     public static class ScanNet {
 
-
         public static Machine escaneaIps(InetAddress ip, int time) throws Exception {
+            
             Machine retorno = new Machine();
             retorno.setIp(ip.getHostName());
 
@@ -88,74 +87,22 @@ public class Utilidades {
                 retorno.setConectado(true);
 
             } else {
-                retorno.setIp(ip.getHostName());
 
                 int[] puertos = {135, 139, 22, 11, 80};
 
                 for (int num : puertos) {
                     try {
-
                         Socket socket = new Socket();
                         socket.connect(new InetSocketAddress(ip.getHostAddress(), num), 200);
                         socket.close();
                         retorno.setConectado(true);
-
                         break;
                     } catch (IOException e1) {
-
                         retorno.setConectado(false);
                     }
-
                 }
-
             }
-
             return retorno;
-        }
-
-        public static int[] rangeFromCidr(String cidrIp) {
-            int maskStub = 1 << 31;
-            String[] atoms = cidrIp.split("/");
-            int mask = Integer.parseInt(atoms[1]);
-            System.out.println(mask);
-
-            int[] result = new int[2];
-            result[0] = InetRange.ipToInt(atoms[0]) & (maskStub >> (mask - 1)); // lower bound
-            result[1] = InetRange.ipToInt(atoms[0]); // upper bound
-            System.out.println(InetRange.intToIp(result[0]));
-            System.out.println(InetRange.intToIp(result[1]));
-
-            return result;
-        }
-
-        public static class InetRange {
-            public static int ipToInt(String ipAddress) {
-                try {
-                    byte[] bytes = InetAddress.getByName(ipAddress).getAddress();
-                    int octet1 = (bytes[0] & 0xFF) << 24;
-                    int octet2 = (bytes[1] & 0xFF) << 16;
-                    int octet3 = (bytes[2] & 0xFF) << 8;
-                    int octet4 = bytes[3] & 0xFF;
-                    int address = octet1 | octet2 | octet3 | octet4;
-
-                    return address;
-                } catch (Exception e) {
-                    e.printStackTrace();
-
-                    return 0;
-                }
-            }
-
-            public static String intToIp(int ipAddress) {
-                int octet1 = (ipAddress & 0xFF000000) >>> 24;
-                int octet2 = (ipAddress & 0xFF0000) >>> 16;
-                int octet3 = (ipAddress & 0xFF00) >>> 8;
-                int octet4 = ipAddress & 0xFF;
-
-                return new StringBuffer().append(octet1).append('.').append(octet2)
-                        .append('.').append(octet3).append('.')
-                        .append(octet4).toString();
-            }
         }
     }
 }
