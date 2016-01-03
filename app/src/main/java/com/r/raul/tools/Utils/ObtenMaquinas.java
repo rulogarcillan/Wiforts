c56f397a9dd8fc0571d06f8cfb21257d5f9a2f15
Rulo <raulrcs@gmail.com>
2016-01-03 19:33:58 +0100

    Nuevos dearrollo

--- app/src/main/java/com/r/raul/tools/Utils/ObtenMaquinas.java
+++ app/src/main/java/com/r/raul/tools/Utils/ObtenMaquinas.java
@@ -2,6 +2,7 @@ package com.r.raul.tools.Utils;
 
 import android.app.Activity;
 import android.content.Context;
+import android.net.DhcpInfo;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.widget.ProgressBar;
@@ -17,6 +18,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
@@ -56,22 +59,32 @@ public class ObtenMaquinas extends AsyncTask<Void, Integer, Void> {
 
     @Override
     protected Void doInBackground(Void... params) {
-    	
-    	final ExecutorService es = Executors.newFixedThreadPool(NUMERO_HILOS);
+
+        final ExecutorService es = Executors.newFixedThreadPool(NUMERO_HILOS);
         final List<Future<Machine>> futures = new ArrayList<Future<Machine>>();
-        
+
         WifiManager wifiManager = (WifiManager) ac.getSystemService(Context.WIFI_SERVICE);
         final String macPadre = wifiManager.getConnectionInfo().getBSSID();
         final String macMyDevice = wifiManager.getConnectionInfo().getMacAddress();
         final String gateway = con.parseIP(wifiManager.getDhcpInfo().gateway);
         final String subMask = con.parseIP(wifiManager.getDhcpInfo().netmask);
         final String loacalIp = con.getLocalAddress().getHostAddress();
-
-	//consultamos las conexiones guardadas para la mac padre
+        String prefix = "";
+        //consultamos las conexiones guardadas para la mac padre
         ArrayList<InspectorTable> arrayInspectorTable = consultas.getAllInspectorTableFromMacPadre(macPadre);
-        
-        SubnetUtils utils = new SubnetUtils(gateway, subMask);
-        SubnetUtils.SubnetInfo info = utils.getInfo();
+
+
+        DhcpInfo dhcpInfo = wifiManager.getDhcpInfo();
+        try {
+            InetAddress inetAddress = InetAddress.getByName(con.parseIP(dhcpInfo.ipAddress));
+            NetworkInterface networkInterface = NetworkInterface.getByInetAddress(inetAddress);
+            for (InterfaceAddress address : networkInterface.getInterfaceAddresses()) {
+                prefix = String.valueOf(address.getNetworkPrefixLength());
+            }
+        } catch (IOException e) {
+
+        }
+
 
 
 
@@ -107,8 +120,19 @@ public class ObtenMaquinas extends AsyncTask<Void, Integer, Void> {
         String subnetMask = "/"+networkInterface.getInterfaceAddresses().get(0).getNetworkPrefixLength();
         System.out.println(ipAddress + subnetMask);*/
 
-        String[] addresses = utils.getInfo().getAllAddresses();
-	
+        SubnetUtils utils = new SubnetUtils(gateway, subMask);
+        SubnetUtils.SubnetInfo info = utils.getInfo();
+        String[] addresses;
+        try {
+            addresses = utils.getInfo().getAllAddresses();
+            LOGE("BIEN");
+        } catch (Exception e) {
+            LOGE("MAL");
+            utils = new SubnetUtils(gateway + "/" + prefix);
+            addresses = utils.getInfo().getAllAddresses();
+        }
+
+
         for (String ip : addresses) {
             InetAddress ipA = null;
             try {
@@ -135,45 +159,45 @@ public class ObtenMaquinas extends AsyncTask<Void, Integer, Void> {
             if (!isCancelled()) {
                 try {
                     if (f.get().isConectado()) {
-                    	
-                    	Boolean isGateway=false;
-                    	Boolean isInBBDD=false;
-                    	Boolean isMyDevice=false;
-                    	
-                    	LogUtils.LOGI(f.get().getIp());
+
+                        Boolean isGateway = false;
+                        Boolean isInBBDD = false;
+                        Boolean isMyDevice = false;
+
+                        LogUtils.LOGI(f.get().getIp());
 
                         f.get().setMacPadre(macPadre); //padre
                         f.get().setMac(getMacFromArpCache(f.get().getIp())); //propia o hija
 
-                    	if (f.get().getIp().equals(gateway)) {
-                            isGateway=true;
+                        if (f.get().getIp().equals(gateway)) {
+                            isGateway = true;
                             f.get().setTipoImg(Constantes.TIPE_GATEWAY);
-                    	}else if (f.get().getIp().equals(loacalIp)){
-                    	    isMyDevice=true;			
-                    	    f.get().setTipoImg(Constantes.TIPE_DEVICE);
+                        } else if (f.get().getIp().equals(loacalIp)) {
+                            isMyDevice = true;
+                            f.get().setTipoImg(Constantes.TIPE_DEVICE);
                             f.get().setMac(macMyDevice); //propia o hija
                         } else {
                             f.get().setTipoImg(Constantes.TIPE_OTHERS);
                         }
 
                         publishProgress(calculoPercent(tot, addresses.length));
-                    	for(InspectorTable item : arrayInspectorTable){
-                    		if(f.get().getMac().equals(item.getMacdevice())){
-                    			isInBBDD=true;
-                    			f.get().setNombre(item.getNombre());
-                    			f.get().setConocido(item.getFavorito());
-                    			break;
-                    		}
-                    	}
-                    	if (!isInBBDD){
-                    		InspectorTable itemIns = new InspectorTable(f.get().getMac(), macPadre, "", (isGateway || isMyDevice) ? true : false);
-                    		consultas.setItemInspectorTable(itemIns);
-                    		arrayInspectorTable.add(itemIns);
-                    		f.get().setNombre("");
-                    		f.get().setConocido((isGateway||isMyDevice) ? true:false);
-                    	}
-                    	
-                    	//agregamos el nombre del hardware
+                        for (InspectorTable item : arrayInspectorTable) {
+                            if (f.get().getMac().equals(item.getMacdevice())) {
+                                isInBBDD = true;
+                                f.get().setNombre(item.getNombre());
+                                f.get().setConocido(item.getFavorito());
+                                break;
+                            }
+                        }
+                        if (!isInBBDD) {
+                            InspectorTable itemIns = new InspectorTable(f.get().getMac(), macPadre, "", (isGateway || isMyDevice) ? true : false);
+                            consultas.setItemInspectorTable(itemIns);
+                            arrayInspectorTable.add(itemIns);
+                            f.get().setNombre("");
+                            f.get().setConocido((isGateway || isMyDevice) ? true : false);
+                        }
+
+                        //agregamos el nombre del hardware
                         NbtAddress[] nbts = new NbtAddress[0];
                         try {
                             publishProgress(calculoPercent(tot, addresses.length));
@@ -188,15 +212,16 @@ public class ObtenMaquinas extends AsyncTask<Void, Integer, Void> {
                             f.get().setNombre(netbiosname);
                         } catch (UnknownHostException e) {
                             f.get().setNombre("-");
-                            if (isMyDevice){
+                            if (isMyDevice) {
                                 f.get().setNombre(ac.getString(R.string.midevice));
                             }
                             e.printStackTrace();
                         }
 
-                    	f.get().setNombreSoft(consultas.getNameFromMac(f.get().getMac()));
-                        
-                        array.add(f.get());publishProgress(calculoPercent(tot, addresses.length));
+                        f.get().setNombreSoft(consultas.getNameFromMac(f.get().getMac()));
+
+                        array.add(f.get());
+                        publishProgress(calculoPercent(tot, addresses.length));
                     }
 
                 } catch (InterruptedException e) {
@@ -214,38 +239,38 @@ public class ObtenMaquinas extends AsyncTask<Void, Integer, Void> {
 
         return null;
     }
-    
-    
+
+
     private String getMacFromArpCache(String ip) {
-		if (ip == null)
-			return null;
-		BufferedReader br = null;
-		try {
-			br = new BufferedReader(new FileReader("/proc/net/arp"));
-			String line;
+        if (ip == null)
+            return null;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader("/proc/net/arp"));
+            String line;
 
-			while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null) {
 
-				String[] splitted = line.split(" +");
-				if (splitted != null && splitted.length >= 4 && ip.equals(splitted[0])) {
-					String mac = splitted[3];
-					if (mac.matches("..:..:..:..:..:..")) {
-						return mac;
-					} else {
+                String[] splitted = line.split(" +");
+                if (splitted != null && splitted.length >= 4 && ip.equals(splitted[0])) {
+                    String mac = splitted[3];
+                    if (mac.matches("..:..:..:..:..:..")) {
+                        return mac;
+                    } else {
                         return ac.getString(R.string.desconocido);
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				br.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
         return ac.getString(R.string.desconocido);
-	}
+    }
 
 }
