791c5fd3627573a17eb4d4281f5cc5323082deb1
Rulo <raulrcs@gmail.com>
2016-02-03 15:03:17 +0100

    Update ObtenMaquinas.java

--- app/src/main/java/com/r/raul/tools/Utils/ObtenMaquinas.java
+++ app/src/main/java/com/r/raul/tools/Utils/ObtenMaquinas.java
@@ -272,7 +272,33 @@ public class ObtenMaquinas extends AsyncTask<Void, Integer, Void> {
 		}
 
 		array.add(item);
-	}
+		
+		Collections.sort(array, new Comparator<Machine>() {
+	            @Override
+		    public int compare(InetAddress adr1, InetAddress adr2) {
+		        byte[] ba1 = adr1.getAddress();
+		        byte[] ba2 = adr2.getAddress();
+		        
+		        if(ba1.length < ba2.length) return -1;
+		        if(ba1.length > ba2.length) return 1;
+		       
+		        for(int i = 0; i < ba1.length; i++) {
+		            int b1 = unsignedByteToInt(ba1[i]);
+		            int b2 = unsignedByteToInt(ba2[i]);
+		            if(b1 == b2)
+		                continue;
+		            if(b1 < b2)
+		                return -1;
+		            else
+		                return 1;
+		        }
+		        return 0;
+		    }
+		
+		    private int unsignedByteToInt(byte b) {
+		        return (int) b & 0xFF;
+		    }
+	        });
 
 	@Override
 	protected void finalize() throws Throwable {
