54c4bd4d6e27ad6afddd3384891cbae022a4d609
Rulo <raulrcs@gmail.com>
2016-02-03 09:53:58 +0100

    Update PortScan.java

--- app/src/main/java/com/r/raul/tools/Utils2/PortScan.java
+++ app/src/main/java/com/r/raul/tools/Utils2/PortScan.java
@@ -4,78 +4,50 @@ package com.r.raul.tools.Utils2;
 import java.util.ArrayList;
 
 public class PortScan {
-    private ArrayList<Port> activePortList;
+  
     private PortScanCallback portScanCallback;
 
     public PortScan(PortScanCallback portScanCallback) {
         this.portScanCallback = portScanCallback;
     }
-
+    
     public PortScan() {
-
     }
-
-    // scan both stand and common ports
+    
     public void start(String ip) throws Exception {
-        activePortList = new ArrayList<Port>();
-        scanStandardPorts(ip);
-        //scanCommonPorts(ip);
+        
+        ArrayList<Port> standardPorts = getStandardPortList(); //puertos a tratar
+        boolean activo = false;
+        for (Port port : standardPorts) {
+            if (scanPort(ip, port)){
+                activo = true;
+                if (portScanCallback != null) {
+                    portScanCallback.onActivePort(ip);
+                }
+                break;
+            }
+        }
+        if (!activo){
+            if (portScanCallback != null) {
+                    portScanCallback.onActivePort(ip);
+                }
+        }
     }
 
     private ArrayList<Port> getStandardPortList() {
         ArrayList<Port> portList = new ArrayList<>();
         portList.add(new Port(Port.TYPE_HTTP, 80));
-        portList.add(new Port(Port.TYPE_RTSP, 554));
         portList.add(new Port(Port.TYPE_HTTP, 135));
         portList.add(new Port(Port.TYPE_HTTP, 139));
         portList.add(new Port(Port.TYPE_HTTP, 22));
         portList.add(new Port(Port.TYPE_HTTP, 11));
-
-
         return portList;
     }
 
-    private void scanPort(String ip, Port port) throws Exception {
+    private Boolean scanPort(String ip, Port port) throws Exception {
         if (Port.isReachable(ip, port.getValue())) {
-            // System.out.println("Active port added: " + ip + ":" + port);
-            port.isActive();
-            activePortList.add(port);
-
-            if (portScanCallback != null) {
-                portScanCallback.onActivePort(port);
-            }
+            return true;
         }
+        return false;
     }
-
-    private void scanStandardPorts(String ip) throws Exception {
-        ArrayList<Port> standardPorts = getStandardPortList();
-
-        for (Port port : standardPorts) {
-            scanPort(ip, port);
-        }
-    }
-
-    /**
-     * Scan the common ports that are frequently used by Evercam only when HTTP
-     * port(80) or RTSP port(554) is not open
-     */
-    private void scanCommonPorts(String ip) throws Exception {
-        String subIp = ip.substring(ip.lastIndexOf(".") + 1, ip.length());
-        int subIpInt = Integer.parseInt(subIp);
-
-        ArrayList<String> activePortTypes = new ArrayList<>();
-        for (Port port : activePortList) {
-            activePortTypes.add(port.getType());
-        }
-
-        if (!activePortTypes.contains(Port.TYPE_HTTP)) {
-            Port commonHttpPort = new Port(Port.TYPE_HTTP, 8000 + subIpInt);
-            scanPort(ip, commonHttpPort);
-        }
-        if (!activePortTypes.contains(Port.TYPE_HTTP)) {
-            Port commonRtspPort = new Port(Port.TYPE_RTSP, 9000 + subIpInt);
-            scanPort(ip, commonRtspPort);
-        }
-    }
-
 }
