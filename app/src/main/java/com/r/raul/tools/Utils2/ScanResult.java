170973e59298f7e35d3e9803bec4841761f97083
Rulo <raulrcs@gmail.com>
2016-02-03 09:58:04 +0100

    Update ScanResult.java

--- app/src/main/java/com/r/raul/tools/Utils2/ScanResult.java
+++ app/src/main/java/com/r/raul/tools/Utils2/ScanResult.java
@@ -4,5 +4,5 @@ package com.r.raul.tools.Utils2;
 public interface ScanResult {
     public void onActiveIp(String ip);
 
-    public void onIpScanned(String ip);
+    public void onInActiveIp(String ip);
 }
