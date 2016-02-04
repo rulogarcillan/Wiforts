c350cc263ffcb3eb51437781d58ae7dbb2f7c891
Rulo <raulrcs@gmail.com>
2016-02-04 09:18:13 +0100

    Update PortScan.java

--- app/src/main/java/com/r/raul/tools/Utils2/PortScan.java
+++ app/src/main/java/com/r/raul/tools/Utils2/PortScan.java
@@ -39,9 +39,11 @@ public class PortScan {
     private ArrayList<Puerto> getStandardPortList() {
         ArrayList<Puerto> portList = new ArrayList<Puerto>();
         portList.add(new Puerto(80,0));
+        portList.add(new Puerto(22,0));
+        portList.add(new Puerto(443,0));
+        portList.add(new Puerto(25,0));
         portList.add(new Puerto(135,0));
         portList.add(new Puerto(139,0));
-        portList.add(new Puerto(22,0));
         portList.add(new Puerto(11,0));
         return portList;
     } 
